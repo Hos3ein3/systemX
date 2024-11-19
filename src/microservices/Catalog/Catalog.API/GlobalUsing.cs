a1bac16607c3a9c84199ce6d5c55f67ccb642241
Mohammad Hossein BehzadiFard <behzadi.hosein@yahoo.com>
2024-11-19 14:20:32 +0100

    added global using file

---
+++ src/microservices/Catalog/Catalog.API/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Carter;
+global using Mapster;
+global using MediatR;
+global using systemX.CQRS;
+global using Catalog.API.Models;
+global using Marten;
--- src/microservices/Catalog/Catalog.API/Program.cs
+++ src/microservices/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,25 @@
+
+using Weasel.Core;
+
 var builder = WebApplication.CreateBuilder(args);
+// dependency Injection
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddCarter();
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opt.AutoCreateSchemaObjects = builder.Environment.IsDevelopment() ?
+        AutoCreate.CreateOrUpdate : AutoCreate.None;
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//! Configure the HTTP requests pipeline.
+
+//app.ConfiguresApp();
+app.MapCarter();
 
 app.Run();
